b2df185f25eef866aff08dd327aa83efd12168f7
Guilherme B Perao <guilhermebperao.gp@gmail.com>
2023-05-08 17:25:14 -0300

    Requisitos 57, 58 e 59

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,12 +3,18 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+  console.log(email);
   return (
     <>
       <Header
         title="Profile"
         searchIcon={ false }
       />
+      <p data-testid="profile-email">{ email }</p>
+      <button data-testid="profile-done-btn">Done Recipes</button>
+      <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button data-testid="profile-logout-btn">Logout</button>
       <Footer />
     </>
   );
